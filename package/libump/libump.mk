e1a432acd4a2ae563931797aeb13fbd43372671a
lzb <lzb.com>
2019-03-03 00:08:35 +0800

    tiny4412 mali400 r2p4

--- package/libump/libump.mk
+++ package/libump/libump.mk
@@ -4,10 +4,21 @@
 #
 ################################################################################
 
-LIBUMP_VERSION = ec0680628744f30b8fac35e41a7bd8e23e59c39f
+LIBUMP_VERSION = 9d0b7e62f77abd3a02510f7068dec983aa83ed00
 LIBUMP_SITE = $(call github,linux-sunxi,libump,$(LIBUMP_VERSION))
 LIBUMP_LICENSE = Apache-2.0
-LIBUMP_AUTORECONF = YES
+#LIBUMP_AUTORECONF = YES
 LIBUMP_INSTALL_STAGING = YES
+	
+define LIBUMP_BUILD_CMDS
+	cd $(@D) && make
+	cd $(@D) && ln -s libUMP.so libUMP.so.2
+endef
 
-$(eval $(autotools-package))
+define LIBUMP_INSTALL_TARGET_CMDS
+	cp $(@D)/include/ump  $(STAGING_DIR)/usr/include/ -r
+	cp $(@D)/libUMP.so* $(STAGING_DIR)/usr/lib/ 
+	cp $(@D)/libUMP.so* $(TARGET_DIR)/usr/lib/ 
+endef
+
+$(eval $(generic-package))
